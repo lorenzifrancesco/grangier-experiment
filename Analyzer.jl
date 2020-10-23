be755a19229030fa2027a3777121089a3501caba
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2020-10-23 18:37:59 +0200

    Too much improvement to be commented here

--- Analyzer.jl
+++ Analyzer.jl
@@ -1,14 +1,15 @@
 module Analyzer
 using Plots
 using Printf
-import PyPlot
+import Plotly
+import Statistics
+export delay_estimator, main, loader, difference_info, gated_counter
 
-export analyze, main, loader
-
-pyplot()
+Plots.plotly()
 default(show = true)
-PyPlot.clf()
-println(PyPlot.backend)
+# PyPlot.clf()
+# println(PyPlot.backend)
+const machine_time = 80.955e-12
 
 function loader()
     println("Loading...")
@@ -62,108 +63,198 @@ function loader()
     return (tags, k);
 end
 
-function analyze((tags, k))
+function delay_estimator((tags, k); mode = "gate_first")
     println("Analyzing...")
     machine_time = 80.955e-12
-
-    g1 = -1         # BE CAREFUL : NOT A REAL GATE EVENT
-    g2 = tags[3, 1]
     diff1 = Array{Int, 1}(undef, k[1])
     diff2 = Array{Int, 1}(undef, k[2])
     fill!(diff1, 0)
     fill!(diff2, 0)
-    n = 1
-
-    # Retarded gate method - positive diff
-    # for i = 2:k[3]
-    #     while (tags[1, n]<g2 && n<k[1])
-    #         diff1[n] = g2 - tags[1, n]
-    #         n += 1
-    #     end
-    #     g2 = tags[3, i]
-    # end
-
-    # Anticipated gate method - positive diff
-    n = 8
-    for i = 2:k[3]
-        while (tags[1, n]<g2 && n<k[1])
-            diff1[n] = tags[1, n] - g1
-            n += 1
+    if mode == "gate_last"
+        g1 = -1         # BE CAREFUL : NOT A REAL GATE EVENT
+        g2 = tags[3, 1]
+        n = 1
+        # Retarded gate method - positive diff
+        for i = 2:k[3]
+            while (tags[1, n]<g2 && n<k[1])
+                diff1[n] = g2 - tags[1, n]
+                n += 1
+            end
+            g2 = tags[3, i]
         end
-        g1 = g2
-        g2 = tags[3, i]
+
+        g1 = -1         # BE CAREFUL : NOT A REAL GATE EVENT
+        g2 = tags[3, 1]
+        n = 1
+        for i = 2:k[3]
+            while (tags[2, n]<g2 && n<k[2])
+                diff2[n] = g2 - tags[2, n]
+                n += 1
+            end
+            g2 = tags[3, i]
+        end
+    elseif mode == "gate_first"
+        # Anticipated gate method - positive diff
+        g1 = -1         # BE CAREFUL : NOT A REAL GATE EVENT
+        g2 = tags[3, 1]
+        n = 8
+        for i = 2:k[3]
+            while (tags[1, n]<g2 && n<k[1])
+                diff1[n] = tags[1, n] - g1
+                n += 1
+            end
+            g1 = g2
+            g2 = tags[3, i]
+        end
+        diff1 = diff1[8:length(diff1)]
+
+        g1 = -1         # BE CAREFUL : NOT A REAL GATE EVENT
+        g2 = tags[3, 1]
+        n = 8
+        for i = 2:k[3]
+            while (tags[2, n]<g2 && n<k[1])
+                diff2[n] = tags[2, n] - g1
+                n += 1
+            end
+            g1 = g2
+            g2 = tags[3, i]
+        end
+        diff2 = diff2[8:length(diff2)]
+    else
+        # Minimum distance method
+        g1 = -100000000         # BE CAREFUL : NOT A REAL GATE EVENT
+        g2 = tags[3, 1]
+        n = 1
+        for i = 2:k[3]
+            while (tags[1, n]<g2 && n<k[1])
+                if ((tags[1, n] - g1) <  (g2 - tags[1, n]))
+                    diff1[n] = tags[1, n] - g1
+                else
+                    diff1[n] = tags[1, n] - g2
+                end
+                n += 1
+            end
+            g1 = g2
+            g2 = tags[3, i]
+        end
+        g1 = -100000000         # BE CAREFUL : NOT A REAL GATE EVENT
+        g2 = tags[3, 1]
+        n = 1
+        for i = 2:k[3]
+            while (tags[2, n]<g2 && n<k[1])
+                if ((tags[2, n] - g1) <  (g2 - tags[2, n]))
+                    diff2[n] = tags[2, n] - g1
+                else
+                    diff2[n] = tags[2, n] - g2
+                end
+                n += 1
+            end
+            g1 = g2
+            g2 = tags[3, i]
+        end
     end
-    diff1 = diff1[8:length(diff1)]
-    
-    # Minimum distance method
-    # for i = 2:k[3]
-    #     while (tags[1, n]<g2 && n<k[1])
-    #         if ((tags[1, n] - g1) <  (g2 - tags[1, n]))
-    #             diff1[n] = tags[1, n] - g1
-    #         else
-    #             diff1[n] = tags[1, n] - g2
-    #         end
-    #         n += 1
-    #     end
-    #     g1 = g2
-    #     g2 = tags[3, i]
-    # end
 
+    max_delay = 10 # [ns]
+    max_clicks = max_delay * 1e-9/machine_time
+    @printf("PRE-filtering at max delay = %d ns \n ", max_delay)
     # unreal difference filter
+    filter!(x-> (x< max_clicks), diff1)
+    filter!(x-> (x< max_clicks), diff2)
 
-    println("maximum difference CH1     : ", maximum(diff1))
-    println("minimum difference CH1     : ", minimum(diff1))
-    @printf("maximum time difference CH1 (ns)  : %10.4f \n", maximum(diff1)/machine_time * 1e9)
-    @printf("minimum time difference CH1 (ns)  : %10.4f \n", minimum(diff1)/machine_time * 1e9)
+    difference_info(diff1, diff2, k)
+    μ1 = Statistics.mean(diff1)
+    μ2 = Statistics.mean(diff2)
+    σ1 = sqrt(Statistics.var(diff1 .- μ1))
+    σ2 = sqrt(Statistics.var(diff2 .- μ2)) 
 
-    filter!(x-> (x< 25e-9/machine_time), diff1)
-    println("Filtered differences are n. : ", length(diff1))
-    println("Which is ")
+    return [μ1, μ2, σ1, σ2]
+end
 
-    mod = Int(floor(0.2e-9/machine_time))
+function difference_info(diff1, diff2, k)
+    machine_time = 80.955e-12
+    println("Difference Info...")
     max_diff1 = maximum(diff1)
     min_diff1 = minimum(diff1)
-    bin_num = Int(floor((max_diff1-min_diff1) / mod)) + 1
-    bias = Int(floor(-min_diff1/mod))
-    # sort!(diff1)
+    max_diff2 = maximum(diff2)
+    min_diff2 = minimum(diff2)
+    @printf("1) maximum difference      : %10d \n", max_diff1)
+    @printf("1) minimum difference      : %10d \n", min_diff1)
+    @printf("1) maximum time difference  (ns)  : %10.4f \n", max_diff1 * machine_time * 1e9)
+    @printf("1) minimum time difference  (ns)  : %10.4f \n", min_diff1 *machine_time * 1e9)
 
-    hist = Array{Int, 1}(undef, bin_num)
-    println("hist length: ", bin_num)
-    
-    fill!(hist, 0)
+    @printf("2) maximum difference      : %10d \n", max_diff2)
+    @printf("2) minimum difference      : %10d \n", min_diff2)
+    @printf("2) maximum time difference  (ns)  : %10.4f \n", max_diff2*machine_time * 1e9)
+    @printf("2) minimum time difference  (ns)  : %10.4f \n\n", min_diff2*machine_time * 1e9)
+
+    @printf("1) Percentage of accepted hits : %d / %d = %4.2f \n", length(diff1), k[1], length(diff1)/k[1])
+    @printf("2) Percentage of accepted hits : %d / %d = %4.2f\n", length(diff2), k[2], length(diff2)/k[2])
+
+    # Want to show exactly 100 bins in histogram
+    mod = Int(ceil(maximum([length(diff1), length(diff2)]) / 10000000))
+    println("Total differences 1 : ", length(diff1))
+    println("Total differences 2 : ", length(diff2))
+
+    x_delays1 = (min_diff1:max_diff1)*mod*machine_time*1e9
+    x_delays2 = (min_diff2:max_diff2)*mod*machine_time*1e9
+
+    bin_num1 = Int(floor((max_diff1-min_diff1) / mod)) + 1
+    println("bins 1: ", bin_num1)
+    bias1 = Int(floor(-min_diff1/mod))
+    hist1 = Array{Int, 1}(undef, bin_num1)
+    fill!(hist1, 0)
     i = 1
     while (i<=length(diff1))
-        hist[Int(floor((diff1[i] - min_diff1) / mod))+1] += 1
+        hist1[Int(floor((diff1[i] - min_diff1) / mod))+1] += 1
         i += 1
     end
 
-    # for i=1:length(hist)
-    #     @printf("%20.18f - %d \n", i*80e-12, hist[i])
-    # end
-    println("maximum frequency at: ", (findmax(hist)[2]-bias)*mod)
-    if (length(hist)<600)
+    bin_num2 = Int(floor((max_diff2-min_diff2) / mod)) + 1
+    bias2 = Int(floor(-min_diff2/mod))
+    println("bins 2: ", bin_num2)
+    hist2 = Array{Int, 1}(undef, bin_num2)
+    fill!(hist2, 0)
+    i = 1
+    while (i<=length(diff2))
+        hist2[Int(floor((diff2[i] - min_diff2) / mod))+1] += 1
+        i += 1
+    end
+
+    if (length(hist1)<600 && length(hist2)<600)
         println("Plotting...")
-        first_n = length(hist)
-        fig = PyPlot.figure()
-        fig = Plots.bar((-bias:first_n-bias)*mod*machine_time*1e9,
-                         hist[1:first_n],
+        # fig = Plotly.figure()
+        fig = Plots.bar(x_delays1,
+                         hist1,
                          show=true,
                          xlabel = "difference to ** gate event (ns)",
                          ylabel = "Frequency")
+        Plots.bar!(x_delays2, hist2)
         display(fig)
     else
-        println("Showing...")
-        for i=1:length(hist)
-            @printf("%20.10f - %d \n", i, hist[i])
-        end
+        println("Too long to plot...")
+        # for i=1:length(hist1)
+        #     @printf("%20.10f - %d \n", i, hist1[i])
+        # end
+        # for i=1:length(hist2)
+        #     @printf("%20.10f - %d \n", i, hist2[i])
+        # end
     end
-
-    # COINCIDENCES EVALUATION
-
-
-    return 0
 end
 
+# need to decide what method to use
+function gated_counter(tags, μ1, σ1, μ2, σ2)
+    println("Gated counting...")
+    tr_hits = 0
+    ref_hits = 0
+    coincidences = 0
+    n_σ = 3
+    for i=1:k[3]
+        if (-n_σ*σ1 < (tags[3, i] - tags[1, j] - μ1) < n_σ*σ1)
+        end
+        if (-n_σ*σ2 < (tags[3, i] - tags[2, k] - μ2) < n_σ*σ2)
+        end
+    end
+end
 function poisson_pdf(x, mu)
     return (mu)^x/factorial(big(x)) * exp(-mu)
 end
@@ -171,6 +262,7 @@ end
 function bose_einst_pdf(x, mu)
     return 1/(mu + 1) * (mu/(mu+1))^x
 end
+
 
 function main()
     println("Nothing to do...")
