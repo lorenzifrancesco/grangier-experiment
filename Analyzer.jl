1b3678d9efda1a583c867338854ba500f4616c09
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2020-10-23 11:58:10 +0200

    Begin to be useful

--- Analyzer.jl
+++ Analyzer.jl
@@ -3,15 +3,15 @@ using Plots
 using Printf
 import PyPlot
 
-export analyze
+export analyze, main, loader
 
 pyplot()
 default(show = true)
 PyPlot.clf()
 println(PyPlot.backend)
 
-function analyze()
-    println("Bellaaa!")
+function loader()
+    println("Loading...")
     s = "./tags.txt"
     a = readlines(s)
     for y in a
@@ -32,6 +32,8 @@ function analyze()
     fill!(k, 1)
     i=0
     cnt = 0
+    aft = Array{Int, 1}(undef, 3)
+    fill!(aft, 0)
     for i = 1:length(a)
         # if (i>7)
         #     println("condition: ", b[1, i] ," < ", tags[ b[2, i]-1, k[b[2, i]-1] - 1 ], "  ?")
@@ -40,62 +42,92 @@ function analyze()
             tags[ b[2, i]-1, k[b[2, i]-1] ] = b[1, i]
             k[b[2, i] - 1] += 1
         else
-            println("Afterpulse on CH-", b[2, i] - 1)
+            # println("Afterpulse on CH-", b[2, i] - 1)
+            aft[b[2, i] - 1] +=1
         end
     end
-    
+
+    println("Number of valid hits")
+    @printf("\t n. of transmitted hits   : %6d \n", k[1])
+    @printf("\t n. of reflected hits     : %6d \n", k[2])
+    @printf("\t n. of gate hits          : %6d \n", k[3])
+    println("T+R = ", k[1]+k[2], ", G = ", k[3])
+    println("Number of afterpulses:")
+    @printf("\t chan 1 - transmitted (2) : %6d \n", aft[1])
+    @printf("\t chan 2 - reflected (3)   : %6d \n", aft[2])
+    @printf("\t chan 3 - gate (4)        : %6d \n", aft[3])
+    println("Percentage of afterpulses")
+    @printf("\t chan 1 - transmitted (2) : %4.1f %% \n", aft[1]/k[1] * 100)
+    @printf("\t chan 2 - reflected (3)   : %4.1f %% \n", aft[2]/k[2] * 100)
+    @printf("\t chan 3 - gate (4)        : %4.1f %% \n", aft[3]/k[3] * 100)
+    return (tags, k);
+end
+
+function analyze((tags, k))
     println("First 100 triggers on channel 2: ", tags[1, 1:10])
 
     println("Plotting arrival differences to nearest trigger event")
-    g1 = tags[3, 1]
-    g2 = tags[3, 2]
+    g1 = -100000000
+    g2 = tags[3, 1]
     diff1 = Array{Int, 1}(undef, k[1])
     diff2 = Array{Int, 1}(undef, k[2])
     fill!(diff1, 0)
     fill!(diff2, 0)
     n = 1
-    for i = 3:k[3]
+    for i = 2:k[3]
         while (tags[1, n]<g2 && n<k[1])
-            diff1[n] = minimum([abs(tags[1, n] - g1), abs(g2 - tags[1, n])])
+            if ((tags[1, n] - g1) <  (g2 - tags[1, n]))
+                diff1[n] = tags[1, n] - g1
+            else
+                diff1[n] = tags[1, n] - g2
+            end
             n += 1
         end
         g1 = g2
         g2 = tags[3, i]
     end
-    println("maximum difference CH1: ", maximum(diff1)*81e-12)
+    println(diff1[1:30])
+    println("maximum difference CH1: ", maximum(diff1))
+    println("minimum difference CH1: ", minimum(diff1))
+    mod = 100000
     max_diff1 = maximum(diff1)
-
-    sort!(diff1)
+    min_diff1 = minimum(diff1)
+    bin_num = Int(floor((max_diff1-min_diff1) / mod)) + 1
+    bias = Int(floor(-min_diff1/mod))
+    # sort!(diff1)
     diff_advance = 0
-    diff_bin_size = 1000000
-    grouped = Array{Int, 1}(undef, Int(ceil((max_diff1/diff_bin_size))) + 1)
-    fill!(grouped, 0)
-    i = 1
-    j = 1
-    while (i<k[1])
-        count = 0
-        while (diff1[i] <= diff_advance && i<k[1])
-            count += 1
-            i += 1
-        end
-        diff_advance += diff_bin_size
-        grouped[j] = count
-        j += 1
-    end
+    diff_bin_size = 10000
 
-    println("maximum diff count: ", maximum(grouped))
-    hist = Array{Int}(undef, maximum(grouped) + 1)
+    hist = Array{Int, 1}(undef, bin_num)
+    println("hist length: ", bin_num)
+    
     fill!(hist, 0)
     i = 1
-    while (i<=length(grouped))
-        hist[grouped[i] + 1] += 1
+    while (i<=k[1])
+        hist[Int(floor((diff1[i] - min_diff1) / mod))+1] += 1
         i += 1
     end
-    println("Plotting...")
-    first_n = length(hist)
-    fig = PyPlot.figure()
-    fig = Plots.bar(1:first_n, hist[1:first_n], show=true)
-    display(fig)
+
+    # for i=1:length(hist)
+    #     @printf("%20.18f - %d \n", i*80e-12, hist[i])
+    # end
+    println("maximum frequency at: ", (findmax(hist)[2]-bias)*mod)
+    if (length(hist)<600)
+        println("Plotting...")
+        first_n = length(hist)
+        fig = PyPlot.figure()
+        fig = Plots.bar((-bias:first_n-bias)*mod, hist[1:first_n], show=true)
+        display(fig)
+    else
+        println("Showing...")
+        for i=1:length(hist)
+            @printf("%20.10f - %d \n", i, hist[i])
+        end
+    end
+
+    # COINCIDENCES EVALUATION
+
+
     return 0
 end
 
@@ -107,16 +139,7 @@ function bose_einst_pdf(x, mu)
     return 1/(mu + 1) * (mu/(mu+1))^x
 end
 
-function poisson_moments(mu)
-    return [mu, mu, mu, mu]
-end
-
-function bose_ein_moments(mu)
-    return [mu, mu + mu^2, mu + 3*mu^2 + 2*mu^3, mu + 10*mu^2 + 18*mu^3 + 9*mu^4]
-end
-
 function main()
-    inter = 20e-6
-    analyze()
+    println("Nothing to do...")
 end
 end
