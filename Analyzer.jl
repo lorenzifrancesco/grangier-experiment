719fff3cff8a9a2b77fe1bfb939be45f1296cf6a
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2020-11-02 12:10:37 +0100

    Full width gate fix

--- Analyzer.jl
+++ Analyzer.jl
@@ -160,7 +160,7 @@ function delay_estimator((tags, k); mode = "gate_first")
 
     # max_delay = 7.5 # [ns]
     # max_clicks = max_delay * 1e-9/machine_time
-    max_clicks = 80
+    max_clicks = 100
     max_delay = max_clicks * machine_time / 1e-9
     @printf("PRE-filtering at max delay = %d ns \n ", max_delay)
     # unreal difference filter
@@ -177,6 +177,203 @@ function delay_estimator((tags, k); mode = "gate_first")
     σ2 = sqrt(Statistics.var(diff2 .- μ2)) 
 
     return [μ1, σ1, μ2, σ2]
+end
+
+function difference_info(diff1, diff2, k)
+    machine_time = 80.955e-12
+    println("Difference Info...")
+    max_diff1 = maximum(diff1)
+    min_diff1 = minimum(diff1)
+    max_diff2 = maximum(diff2)
+    min_diff2 = minimum(diff2)
+    @printf("1) maximum difference      : %10d \n", max_diff1)
+    @printf("1) minimum difference      : %10d \n", min_diff1)
+    @printf("1) maximum time difference  (ns)  : %10.4f \n", max_diff1 * machine_time * 1e9)
+    @printf("1) minimum time difference  (ns)  : %10.4f \n", min_diff1 *machine_time * 1e9)
+
+    @printf("2) maximum difference      : %10d \n", max_diff2)
+    @printf("2) minimum difference      : %10d \n", min_diff2)
+    @printf("2) maximum time difference  (ns)  : %10.4f \n", max_diff2*machine_time * 1e9)
+    @printf("2) minimum time difference  (ns)  : %10.4f \n\n", min_diff2*machine_time * 1e9)
+
+    @printf("1) Fraction of accepted hits : %d / %d = %4.2f \n", length(diff1), k[1], length(diff1)/k[1])
+    @printf("2) Fraction of accepted hits : %d / %d = %4.2f\n", length(diff2), k[2], length(diff2)/k[2])
+
+    # Want to show exactly 100 bins in histogram
+    mod = Int(ceil(maximum([length(diff1), length(diff2)]) / 1e4)) # TO BE MODIFIED
+
+    # plot clicks 
+    x_delays1 = (min_diff1:mod:max_diff1)
+    x_delays2 = (min_diff2:mod:max_diff2)
+
+    bin_num1 = Int(floor((max_diff1-min_diff1) / mod)) + 1
+    println("bins 1: ", bin_num1)
+    bias1 = Int(floor(-min_diff1/mod))
+    hist1 = Array{Int, 1}(undef, bin_num1)
+    fill!(hist1, 0)
+    i = 1
+    while (i<=length(diff1))
+        hist1[Int(floor((diff1[i] - min_diff1) / mod))+1] += 1
+        i += 1
+    end
+
+    bin_num2 = Int(floor((max_diff2-min_diff2) / mod)) + 1
+    bias2 = Int(floor(-min_diff2/mod))
+    println("bins 2: ", bin_num2)
+    hist2 = Array{Int, 1}(undef, bin_num2)
+    fill!(hist2, 0)
+    i = 1
+    while (i<=length(diff2))
+        hist2[Int(floor((diff2[i] - min_diff2) / mod))+1] += 1
+        i += 1
+    end
+    μ1 = Statistics.mean(diff1)
+    μ2 = Statistics.mean(diff2)
+    σ1 = sqrt(Statistics.var(diff1 .- μ1))
+    σ2 = sqrt(Statistics.var(diff2 .- μ2)) 
+
+    if (length(hist1)<600 && length(hist2)<600)
+        println("Plotting...")
+        # fig = Plotly.figure()
+        n_σ = 2
+        fig = Plots.bar(x_delays1,
+                         hist1,
+                         show=true,
+                         xlabel = "absolute difference from gate event (MTU)",
+                         ylabel = "frequency", 
+                         label = "T", 
+                         size = (600, 400))
+        Plots.bar!(x_delays2, hist2, label = "R")
+        rectangle(w, h, x, y) = Plots.Shape(x .+ [0,w,w,0], y .+ [0,0,h,h])
+
+        recr = rectangle(2*n_σ*σ1, maximum([maximum(hist1), maximum(hist2)]), μ1-n_σ*σ1, 0)
+        rect = rectangle(2*n_σ*σ2, maximum([maximum(hist1), maximum(hist2)]), μ2-n_σ*σ2, 0)
+        # Plots.plot!(recr, linewidth = 2, opacity = 0.1, color=:blue, label=nothing)
+        # Plots.plot!(rect, linewidth = 2, opacity = 0.1, color=:red, label=nothing)
+
+        display(fig)
+        savefig("./images/delays.pdf")
+    else
+        println("Too long to plot...")
+    end
+end
+
+# need to decide what method to use -> we use  GATE -> REFLECTED -> TRANSMITTED
+function gated_counter((tags, k), params; mode = "confidence")
+    println("Gated counting...")
+    μ1 = params[1]
+    σ1 = params[2]
+    μ2 = params[3]
+    σ2 = params[4]
+
+    @printf("mean reflected   : %6.4f \n", params[1])
+    @printf("stdd reflected   : %6.4f \n", params[2])
+    @printf("mean tramsmitted : %6.4f \n", params[3])
+    @printf("stdd tramsmitted : %6.4f \n", params[4])
+    # N_1 = length(tags[3, :])
+    N_1 = 0
+    intervals = [2]
+    # Gate function (not counting with multiple hits)
+    for n_σ in intervals
+        max_clicks = 100
+        x = 1
+        r_hit = false
+        refl = 0
+        multiple_refl = 0
+        y = 1
+        t_hit = false
+        tran = 0
+        multiple_tran = 0
+        coincidences = 0
+        if (mode == "confidence") 
+            for i=1:length(tags[3, :])-1
+                r_hit = false
+                t_hit = false  
+                while  tags[1, x] < -n_σ*σ1 + tags[3, i] + μ1
+                    x += 1
+                end
+                while -n_σ*σ1 + tags[3, i] + μ1 <= tags[1, x] < +n_σ*σ1 + tags[3, i] + μ1 && tags[1, x] < tags[3, i+1] 
+                    r_hit = true
+                    x += 1
+                end
+                if r_hit
+                    refl += 1
+                end
+
+                while  tags[2, y] < -n_σ*σ2 + tags[3, i] + μ2 
+                    y += 1
+                end
+                while -n_σ*σ2 + tags[3, i] + μ2 <= tags[2, y] < +n_σ*σ2 + tags[3, i] + μ2  && tags[2, y] < tags[3, i+1]
+                    t_hit = true
+                    y += 1
+                end
+                if t_hit
+                    tran += 1
+                end
+                if r_hit && t_hit
+                    coincidences += 1
+                end
+                if r_hit || t_hit
+                    N_1 += 1
+                end
+            end
+        else
+            for i=1:length(tags[3, :])-1
+                r_hit = false
+                t_hit = false 
+                while tags[1, x] < tags[3, i]
+                    x += 1
+                end
+                while tags[3, i] < tags[1, x] <= tags[3, i] + max_clicks
+                    r_hit = true
+                    x += 1
+                end
+                if r_hit
+                    refl += 1
+                end
+
+                while tags[2, y] < tags[3, i]
+                    y += 1
+                end
+                while tags[3, i] < tags[2, y] <= tags[3, i] + max_clicks
+                    t_hit = true
+                    y += 1
+                end
+                if t_hit
+                    tran += 1
+                end
+                if r_hit && t_hit
+                    coincidences += 1
+                end
+                if r_hit || t_hit
+                    N_1 += 1
+                end
+            end
+        end
+        @printf("Measurement with ± σ confidence \n")
+        println("sigma = ", n_σ)
+        prob_refl = refl / N_1
+        prob_tran = tran / N_1
+        prob_triple = coincidences / N_1
+        α = prob_triple/ (prob_refl * prob_tran)
+        @printf("\t gate         hits :  %9d \n", N_1)
+        @printf("\t reflected    hits :  %9d \n", refl)
+        @printf("\t transmitted  hits :  %9d \n", tran)
+        @printf("\t coincidences hits :  %9d \n", coincidences)
+        @printf(" ----------------------\n")
+        @printf("\t P[double]         : %9.8f \n", prob_refl + prob_tran - 2 *prob_triple)
+        @printf("\t P[triple]         : %9.8f \n", prob_triple)
+        @printf("\t Alpha             : %9.8f \n", α)
+    end
+end
+
+function config()
+    Plots.gr()
+    Plots.default(size=(600, 400), 
+    guidefont=("times", 14), 
+    legendfont=("times", 14),
+    tickfont=("times", 14)
+    )
 end
 
 function single_chan_stat((tags, k))
@@ -242,189 +439,5 @@ function bose_ein_moments(mu)
             sigma^2,
             (mu + 3*mu^2 + 2*mu^3)/sigma^3,
             (mu + 10*mu^2 + 18*mu^3 + 9*mu^4)/sigma^4]
-end
-
-function difference_info(diff1, diff2, k)
-    machine_time = 80.955e-12
-    println("Difference Info...")
-    max_diff1 = maximum(diff1)
-    min_diff1 = minimum(diff1)
-    max_diff2 = maximum(diff2)
-    min_diff2 = minimum(diff2)
-    @printf("1) maximum difference      : %10d \n", max_diff1)
-    @printf("1) minimum difference      : %10d \n", min_diff1)
-    @printf("1) maximum time difference  (ns)  : %10.4f \n", max_diff1 * machine_time * 1e9)
-    @printf("1) minimum time difference  (ns)  : %10.4f \n", min_diff1 *machine_time * 1e9)
-
-    @printf("2) maximum difference      : %10d \n", max_diff2)
-    @printf("2) minimum difference      : %10d \n", min_diff2)
-    @printf("2) maximum time difference  (ns)  : %10.4f \n", max_diff2*machine_time * 1e9)
-    @printf("2) minimum time difference  (ns)  : %10.4f \n\n", min_diff2*machine_time * 1e9)
-
-    @printf("1) Fraction of accepted hits : %d / %d = %4.2f \n", length(diff1), k[1], length(diff1)/k[1])
-    @printf("2) Fraction of accepted hits : %d / %d = %4.2f\n", length(diff2), k[2], length(diff2)/k[2])
-
-    # Want to show exactly 100 bins in histogram
-    mod = Int(ceil(maximum([length(diff1), length(diff2)]) / 1e4)) # TO BE MODIFIED
-
-    # plot clicks 
-    x_delays1 = (min_diff1:mod:max_diff1)
-    x_delays2 = (min_diff2:mod:max_diff2)
-
-    bin_num1 = Int(floor((max_diff1-min_diff1) / mod)) + 1
-    println("bins 1: ", bin_num1)
-    bias1 = Int(floor(-min_diff1/mod))
-    hist1 = Array{Int, 1}(undef, bin_num1)
-    fill!(hist1, 0)
-    i = 1
-    while (i<=length(diff1))
-        hist1[Int(floor((diff1[i] - min_diff1) / mod))+1] += 1
-        i += 1
-    end
-
-    bin_num2 = Int(floor((max_diff2-min_diff2) / mod)) + 1
-    bias2 = Int(floor(-min_diff2/mod))
-    println("bins 2: ", bin_num2)
-    hist2 = Array{Int, 1}(undef, bin_num2)
-    fill!(hist2, 0)
-    i = 1
-    while (i<=length(diff2))
-        hist2[Int(floor((diff2[i] - min_diff2) / mod))+1] += 1
-        i += 1
-    end
-    μ1 = Statistics.mean(diff1)
-    μ2 = Statistics.mean(diff2)
-    σ1 = sqrt(Statistics.var(diff1 .- μ1))
-    σ2 = sqrt(Statistics.var(diff2 .- μ2)) 
-
-    if (length(hist1)<600 && length(hist2)<600)
-        println("Plotting...")
-        # fig = Plotly.figure()
-        n_σ = 2
-        fig = Plots.bar(x_delays1,
-                         hist1,
-                         show=true,
-                         xlabel = "absolute difference from gate event (MTU)",
-                         ylabel = "Frequency", 
-                         label = "Transmitted delay", 
-                         size = (1000, 600))
-        Plots.bar!(x_delays2, hist2, label = "Reflected delay")
-        rectangle(w, h, x, y) = Plots.Shape(x .+ [0,w,w,0], y .+ [0,0,h,h])
-
-        recr = rectangle(2*n_σ*σ1, maximum([maximum(hist1), maximum(hist2)]), μ1-n_σ*σ1, 0)
-        rect = rectangle(2*n_σ*σ2, maximum([maximum(hist1), maximum(hist2)]), μ2-n_σ*σ2, 0)
-        # Plots.plot!(recr, linewidth = 2, opacity = 0.1, color=:blue, label=nothing)
-        # Plots.plot!(rect, linewidth = 2, opacity = 0.1, color=:red, label=nothing)
-
-        display(fig)
-        savefig("./images/delays.pdf")
-    else
-        println("Too long to plot...")
-    end
-end
-
-# need to decide what method to use -> we use  GATE -> REFLECTED -> TRANSMITTED
-function gated_counter((tags, k), params; mode = "full-width")
-    println("Gated counting...")
-    μ1 = params[1]
-    σ1 = params[2]
-    μ2 = params[3]
-    σ2 = params[4]
-
-    @printf("mean reflected   : %6.4f \n", params[1])
-    @printf("stdd reflected   : %6.4f \n", params[2])
-    @printf("mean tramsmitted : %6.4f \n", params[3])
-    @printf("stdd tramsmitted : %6.4f \n", params[4])
-    N_1 = length(tags[3, :])
-    intervals = [6]
-    # Gate function (not counting with multiple hits)
-    for n_σ in intervals
-        x = 1
-        r_hit = false
-        refl = 0
-        multiple_refl = 0
-        y = 1
-        t_hit = false
-        tran = 0
-        multiple_tran = 0
-        coincidences = 0
-        if (mode == "confidence") 
-            for i=1:length(tags[3, :])-1
-                r_hit = false
-                t_hit = false  
-                while  tags[1, x] < -n_σ*σ1 + tags[3, i] + μ1
-                    x += 1
-                end
-                while -n_σ*σ1 + tags[3, i] + μ1 <= tags[1, x] < +n_σ*σ1 + tags[3, i] + μ1 && tags[1, x] < tags[3, i+1] 
-                    r_hit = true
-                    x += 1
-                end
-                if r_hit
-                    refl += 1
-                end
-
-                while  tags[2, y] < -n_σ*σ2 + tags[3, i] + μ2 
-                    y += 1
-                end
-                while -n_σ*σ2 + tags[3, i] + μ2 <= tags[2, y] < +n_σ*σ2 + tags[3, i] + μ2  && tags[2, y] < tags[3, i+1]
-                    t_hit = true
-                    y += 1
-                end
-                if t_hit
-                    tran += 1
-                end
-                if r_hit && t_hit
-                    coincidences += 1
-                end
-            end
-        else
-            for i=1:length(tags[3, :])-1
-                r_hit = false
-                t_hit = false 
-                while tags[1, x] < tags[3, i]
-                    x += 1
-                end
-                while tags[3, i] < tags[1, x] <= tags[3, i+1] 
-                    r_hit = true
-                    x += 1
-                end
-                if r_hit
-                    refl += 1
-                end
-
-                while tags[2, y] < tags[3, i]
-                    y += 1
-                end
-                while tags[3, i] < tags[2, y] <= tags[3, i+1]
-                    t_hit = true
-                    y += 1
-                end
-                if t_hit
-                    tran += 1
-                end
-                if r_hit && t_hit
-                    coincidences += 1
-                end
-            end
-        end
-        @printf("Measurement with ciao confidence \n")
-        prob_refl = refl / N_1
-        prob_tran = tran / N_1
-        prob_triple = coincidences / N_1
-        α = prob_triple/ (prob_refl * prob_tran)
-        @printf("\t gate         hits :  %9d \n", N_1)
-        @printf("\t reflected    hits :  %9d \n", refl)
-        @printf("\t transmitted  hits :  %9d \n", tran)
-        @printf("\t coincidences hits :  %9d \n", coincidences)
-        @printf(" ----------------------\n")
-        @printf("\t P[double]         : %9.8f \n", prob_refl + prob_tran)
-        @printf("\t P[triple]         : %9.8f \n", prob_triple)
-        @printf("\t Alpha             : %9.8f \n", α)
-    end
-end
-
-function config()
-    Plots.plotly()
-    Plots.default(guidefont=("times", 14), legendfont=("times", 12))
 end
 end
